e89cf602dc94af6328be31ddfc9a0b38babd0058
plumliil <476453574@qq.com>
2022-10-22 09:06:33 +0800

    the components entry

---
+++ components/index.ts
@@ -0,0 +1,20 @@
+// import liiButton from "./button";
+import liiInput from "./input";
+// import liiTextarea from "./textarea";
+// import liiSpace from "./space";
+// import liiSelect from "./select";
+// import liiRadio from "./radio";
+// import Message from "./message";
+// import liiCollapse from "./collapse";
+// import liiProgress from "./progress";
+import liiBadge from "./badge";
+// import liiLoading from "./loading";
+// import liiUpload from "./upload";
+
+
+
+
+
+const components: any[] = [liiInput, liiBadge];
+
+export { components };
