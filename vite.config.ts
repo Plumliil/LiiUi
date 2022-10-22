92cd7d64237a6fd897f62a2c904c052b7e809853
plumliil <476453574@qq.com>
2022-10-22 09:07:23 +0800

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: './index.ts',
+      name: 'lii-ui'
+    },
+    rollupOptions:{
+      external:['vue'],
+      output:{
+        globals:{
+          vue:'Vue'
+        }
+      }
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+        additionalData: `@import "${path.resolve(__dirname, 'common/styles/base.less')}";`
+      }
+    }
+  }
 })
