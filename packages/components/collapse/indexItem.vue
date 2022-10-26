d1454a7cc7d50e8d6ec904a1594fbf4e3ac051d3
plumliil <476453574@qq.com>
2022-10-26 17:47:19 +0800

    collapse component

--- packages/components/collapse/indexItem.vue
+++ packages/components/collapse/indexItem.vue
@@ -11,14 +11,9 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: "liiCollapseItem",
-};
-</script>
-<script setup>
+<script setup lang="ts">
 import { ref } from "@vue/reactivity";
-import { computed, inject, onMounted } from "@vue/runtime-core";
+import { inject } from "@vue/runtime-core";
 const props = defineProps({
   title: {
     type: String,
@@ -27,10 +22,16 @@ const props = defineProps({
     type: [String, Number],
   },
 });
-const PARENT_PROVIDE = "parentProvide";
-const parentProps = inject(PARENT_PROVIDE);
-let isShow = ref(parentProps.value.indexOf(props.name) > -1 ? true : false);
-const changeActive = (v) => {
+const PARENT_PROVIDE: string = "parentProvide";
+const parentProps: any = inject(PARENT_PROVIDE);
+let isShow = ref<boolean>(
+  parentProps.value.indexOf(props.name) > -1 ? true : false
+);
+const changeActive = (v): void => {
   isShow.value = !isShow.value;
 };
 </script>
+
+<!-- <style lang="less" scoped>
+@import url("@/packages/theme-chalk/components/collapse.less");
+</style> -->
